221c80020ffc81a4c7af547ad0a7351c8bbed45a
Jatin Verma <jatin28jat@gmail.com>
2024-07-27 00:22:07 +0530

    Socket URL Updated

--- frontend/src/Context/SocketContext.jsx
+++ frontend/src/Context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:10000", {
+			const socket = io("https://messengio.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
